6c225c4739ea2150a62a4f35752253237615798e
XiNGRZ <hi@xingrz.me>
2022-08-25 21:38:23 +0800

    [nanoDAP] Use TDO/TDI for SWCLK/SWDIO

--- source/board/override_muselab_nanodap/IO_Config_Override.h
+++ source/board/override_muselab_nanodap/IO_Config_Override.h
@@ -54,17 +54,17 @@ COMPILER_ASSERT(DAPLINK_HIC_ID == DAPLINK_HIC_ID_STM32F103XB);
 #define nRESET_PIN_Bit               0
 
 //SWD
-#define SWCLK_TCK_PIN_PORT           GPIOB
-#define SWCLK_TCK_PIN                GPIO_PIN_13
-#define SWCLK_TCK_PIN_Bit            13
+#define SWCLK_TCK_PIN_PORT           GPIOA
+#define SWCLK_TCK_PIN                GPIO_PIN_5
+#define SWCLK_TCK_PIN_Bit            5
 
-#define SWDIO_OUT_PIN_PORT           GPIOB
-#define SWDIO_OUT_PIN                GPIO_PIN_14
-#define SWDIO_OUT_PIN_Bit            14
+#define SWDIO_OUT_PIN_PORT           GPIOA
+#define SWDIO_OUT_PIN                GPIO_PIN_7
+#define SWDIO_OUT_PIN_Bit            7
 
-#define SWDIO_IN_PIN_PORT            GPIOB
-#define SWDIO_IN_PIN                 GPIO_PIN_12
-#define SWDIO_IN_PIN_Bit             12
+#define SWDIO_IN_PIN_PORT            GPIOA
+#define SWDIO_IN_PIN                 GPIO_PIN_6
+#define SWDIO_IN_PIN_Bit             6
 
 //LEDs
 //USB status LED
